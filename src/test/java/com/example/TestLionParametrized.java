9ca0bdc432e59fc700091875f9af432b8f57749a
Mike G <kukozi47@gmail.com>
2023-07-16 13:16:07 +0300

    refactor: move parametrized Lion test to a separate class

---
+++ src/test/java/com/example/TestLionParametrized.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class TestLionParametrized {
+    private Lion testLion;
+    private String sex;
+    private boolean hasMane;
+    Feline testFeline = new Feline();
+
+    public TestLionParametrized(String sex, boolean hasMane) {
+        this.sex = sex;
+        this.hasMane = hasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] lionTestSet() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+        };
+    }
+
+    @Test
+    public void testDoesHaveMane() throws Exception{
+        Lion testLion = new Lion(sex, testFeline);
+        Assert.assertEquals("Wrong int returned", hasMane, testLion.doesHaveMane());
+    }
+
+}
