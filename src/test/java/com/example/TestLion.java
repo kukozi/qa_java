f476f4ccfecda115acf0bddde07412a4b2ab71d5
Mike G <kukozi47@gmail.com>
2023-07-16 13:39:49 +0300

    refactor: modify Lion tests

--- src/test/java/com/example/TestLion.java
+++ src/test/java/com/example/TestLion.java
@@ -4,58 +4,46 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
-@RunWith(Parameterized.class)
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TestLion {
+    private String sex = "Самец";
+    @Mock
+    private Feline testFeline;
     private Lion testLion;
-    private String sex;
-    private boolean hasMane;
-
-    public TestLion(String sex, boolean hasMane) {
-        this.sex = sex;
-        this.hasMane = hasMane;
-    }
-
     @Before
     public void initLion() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        testLion = new Lion(sex);
+        testLion = new Lion(sex, testFeline);
     }
-
-    @Parameterized.Parameters
-    public static Object[][] lionTestSet() {
-        return new Object[][]{
-                {"Самец", true},
-                {"Самка", false}
-        };
-    }
-
     @Test
     public void testGetKittens() {
+        Mockito.when(testFeline.getKittens()).thenReturn(1);
         int expectedResult = 1;
         int actualResult = testLion.getKittens();
         Assert.assertEquals("Wrong int returned", expectedResult, actualResult);
     }
 
     @Test
-    public void testDoesHaveMane() {
-        Assert.assertEquals("Wrong int returned", hasMane, testLion.doesHaveMane());
-    }
-
-    @Test
     public void testGetFood() throws Exception {
-        Assert.assertEquals("Wrong list returned", List.of("Животные", "Птицы", "Рыба"), testLion.getFood());
+        List<String> foodList = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(testFeline.getFood("Хищник")).thenReturn(foodList);
+        Assert.assertEquals("Wrong list returned", foodList, testLion.getFood());
     }
 
     @Test
     public void testLionWrongSexPassed() throws Exception {
-        String exceptionMessage = "Используйте допустимые значения пола животного - самей или самка";
-        Exception exception = Assert.assertThrows(Exception.class, () -> new Lion("Лев-квир"));
+        String exceptionMessage = "Используйте допустимые значения пола животного - самец или самка";
+        Exception exception = Assert.assertThrows(Exception.class, () -> new Lion("Лев-квир", testFeline));
         Assert.assertEquals("Exception handled improperly", exceptionMessage, exception.getMessage());
     }
 
